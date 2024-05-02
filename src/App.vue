f89af8a995dce4cd1ce87644277b69323b8b20e0
olga_demina <olga.demina1410@gmail.com>
2024-05-02 10:24:18 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script>
+import AppBanner from './components/AppBanner.vue';
+
+export default {
+  components: {
+    AppBanner
+  },
+  data() {
+    return {
+      title: "Ciao"
+    }
+  }
+}
+</script>
+
+<template>
+  <h1>{{ title }}</h1>
+  <AppBanner />
+</template>
+
+<style>
+</style>
---
+++ src/components/AppBanner.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="banner">
+    <h2>Titolo</h2>
+  </div>
+</template>
+
+<style>
+.banner {
+  height: 60vh;
+  background-color: lightblue;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center
+}
+</style>
